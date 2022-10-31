f5fd4589ca1440b7e9c89b41b5cf017b03b3a6f9
AndreSci <scitecever@gmail.com>
2022-10-31 12:42:18 +0300

    fix

--- database/driver/rest_driver.py
+++ database/driver/rest_driver.py
@@ -11,13 +11,14 @@ class ConDriver:
     def send_photo(self, json_data: json, logger: Logger) -> str:
         ret_value = "ERROR"
         try:
-            # result = requests.post(f"{self.settings_ini['dr_host']}:{self.settings_ini['dr_port']}", json=json_data)
             result = requests.post(f"http://{self.settings_ini['dr_host']}:{self.settings_ini['dr_port']}/addFace",
                                    data=json_data)
-            # params = json_data)
 
             json_data = result.json()
             ret_value = json_data["RESULT"]
+
+            if ret_value == "ERROR":
+                logger.add_log(f"ERROR\tОшибка на драйвере - {json_data['DATA']}")
 
         except Exception as ex:
             logger.add_log(f"ERROR\tИсключение вызвало связь с Драйвером - {ex}")
