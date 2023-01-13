c943ca6cf9a224b633faf7ab561c087ca2a38251
AndreSci <scitecever@gmail.com>
2023-01-13 17:13:08 +0300

    add email class

---
+++ misc/send_email.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class SendEMAIL:
+    """ Класс предназначен для отправки email сообщений посетителю """
+
+    def __init__(self, set_ini: dict):
+        """ Принимает словарь с настройками из settings.ini """
+        self.host = set_ini["sms_host"]
+        self.port = set_ini["sms_port"]
+
+    def send_sms(self, email_name, text):
+        res = requests.post(f"http://{self.host}:{self.port}/send_email/?femail={email_name}&ftext={text}")
+
+        return res
--- misc/send_sms.py
+++ misc/send_sms.py
@@ -2,8 +2,10 @@ import requests
 
 
 class SendSMS:
-    """ Принимает словарь с настройками из settings.ini """
+    """ Класс предназначен для отправки sms сообщений содержащий шестизначный код """
+
     def __init__(self, set_ini: dict):
+        """ Принимает словарь с настройками из settings.ini """
         self.host = set_ini["sms_host"]
         self.port = set_ini["sms_port"]
 
