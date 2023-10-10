f9b668fab41b69dd40a5cdf0d06d5798f30e34d2
AndreSci <scitecever@gmail.com>
2023-10-10 15:04:02 +0300

    FIX CONVERT_WORDS

--- misc/allowed_words.py
+++ misc/allowed_words.py
@@ -5,8 +5,12 @@ def convert_word(word: str) -> str:
     """ Функция убирает лишние спец символы """
 
     if word:
-        result = re.sub("[^А-Яа-яA-Za-z1-9]", "", word)
+        result = re.sub("[^А-Яа-яA-Za-z0-9]", "", word)
     else:
         result = word
 
     return result
+
+
+if __name__ == "__main__":
+    print(convert_word("1236127368273000"))
