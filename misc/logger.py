3606e0d3a7a85c0bf7bc269415924075c9322c76
AndreSci <scitecever@gmail.com>
2024-01-15 17:22:03 +0300

    remove comment

--- misc/logger.py
+++ misc/logger.py
@@ -72,14 +72,12 @@ class Logger(metaclass=SingletonBaseClass):
             if test_dir(LOGGER_PATH):
 
                 # if LOGGER_PATH[-1] == '\\' or LOGGER_PATH[-1] == '/':
-                #     pass  # Захотелось использовать pass
+                #     pass
                 # else:
                 #     log_path = LOGGER_PATH + '/'
 
                 with self.log_guard:  # Защищаем поток
 
-                    # if print_it:
-                    #     print(date_time + "\t" + text)
                     if print_it:
                         if 'ERROR' == text[:5]:
                             print(f"{BColors.col_fail}{date_time}\t{text}{BColors.col_endc}")
