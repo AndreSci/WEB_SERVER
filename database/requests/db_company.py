0a2baf39ac5cb160d67211734e75b83a7f50f597
AndreSci <scitecever@gmail.com>
2024-03-07 16:45:32 +0300

    added AddCompanyContact 100% added GetCompanyContact 100% need testing

--- database/requests/db_company.py
+++ database/requests/db_company.py
@@ -102,12 +102,91 @@ class CompanyDB:
                 request_res = cur.fetchone()
 
                 if request_res:
-                    ret_value['RESULT'] = "SUCCESS"
-                    ret_value['DATA'] = request_res
+
+                    cur.execute(f"select * from sac3.tcompany where FINN = %s", (str(data.get('FINN')), ))
+
+                    inn_result = cur.fetchone()
+
+                    if inn_result:
+
+                        cur.execute(f"insert into sac3.tcompanycontact "
+                                    f"(FCompanyID, FPhone, FOfficeNumber, FOKVED, FName, FDateCreate) "
+                                    f"values "
+                                    f"({inn_result.get('FID')}, '{data.get('FPhone')}', "
+                                    f"'{data.get('FOfficeNumber')}', '{data.get('FOKVED')}',"
+                                    f"'{data.get('FName')}',now());")
+
+                        res = cur.rowcount
+
+                        if res == 1:
+                            ret_value['RESULT'] = "SUCCESS"
+                            connection.commit()
+                        else:
+                            ret_value['DESC'] = f"Сервер SQL ответил кол. изменений = {res}"
+                    else:
+                        ret_value['DESC'] = f"Не удалось найти ИНН: {data.get('FINN')}"
                 else:
                     ret_value['DESC'] = "Не удалось найти компанию"
 
         except Exception as ex:
+            ret_value['DESC'] = f"Ошибку вызвало: {ex}"
+            LOGGER.exception(f"Ошибку вызвало: {ex}")
+
+
+        return ret_value
+
+    @staticmethod
+    def get_contact(data: dict) -> dict:
+        """ Функция предоставляет FID из БД sac3.taccount """
+
+        ret_value = RET_VALUE
+
+        try:
+            # Создаем подключение
+            connection = connect_db()
+
+            with connection.cursor() as cur:
+
+                # Проверяем компанию и пользователя на доступность
+                cur.execute(f"select * from sac3.taccount, sac3.tcompany "
+                                f"where FCompanyID = tcompany.FID "
+                                f"and taccount.FID = %s "
+                                f"and tcompany.FActivity = 1 "
+                                f"and taccount.FActivity = 1", (data.get('FAccountID'), ))
+
+                request_res = cur.fetchone()
+
+                if request_res:
+
+                    cur.execute(f"select * from sac3.tcompany where FINN = %s", (str(data.get('FINN')), ))
+
+                    inn_result = cur.fetchone()
+
+                    if inn_result:
+
+                        cur.execute(f"select * from sac3.tcompanycontact "
+                                    f"where FCompanyID = {inn_result.get('FID')} order by FID desc")
+
+                        res = cur.fetchone()
+
+                        if res:
+                            ret_value['RESULT'] = "SUCCESS"
+                            ret_value['DATA'] = {'name': res.get('FName'),
+                                                'OKVED': res.get('FOKVED'),
+                                                'office': res.get('FOfficeNumber'),
+                                                'phone': res.get('FPhone'),
+                                                 'date_create': str(res.get('FDateCreate'))
+                                                }
+                        else:
+                            ret_value['DESC'] = f"Не удалось найти контакты по данному ИНН: {data.get('FINN')}"
+                    else:
+                        ret_value['DESC'] = f"Не удалось найти ИНН: {data.get('FINN')}"
+
+                else:
+                    ret_value['DESC'] = "Не удалось найти компанию"
+
+        except Exception as ex:
+
             ret_value['DESC'] = f"Ошибку вызвало: {ex}"
             LOGGER.exception(f"Ошибку вызвало: {ex}")
 
--- routes/company.py
+++ routes/company.py
@@ -1,13 +1,9 @@
 import requests
 import datetime
 from flask import Blueprint, request, jsonify
-from misc.consts import ALLOW_IP, ERROR_ACCESS_IP, ConstControl
-from misc.allowed_words import convert_word
-from database.requests.db_company import CompanyDB
-from database.requests.db_get_card_holders import CardHoldersDB
-from misc.consts import GUEST_DAYS
+from misc.consts import ALLOW_IP, ERROR_ACCESS_IP
 from misc.logger import Logger
-
+from database.requests.db_company import CompanyDB
 
 logger = Logger()
 company_blue = Blueprint('company_blue', __name__, template_folder='templates', static_folder='static')
@@ -36,9 +32,35 @@ def add_comp_contact():
                     'FOfficeNumber': json_request.get('office'),
                     'FPhone': json_request.get('phone')
                     }
-            print(data)
+            ret_value = CompanyDB.add_contact(data)
 
-            ret_value = {"RESULT": "SUCCESS"}
+        else:
+           logger.error(f"Не удалось получить данные из JSON: {json_request}")
+           ret_value['DESC'] = "Не удалось получить данные из JSON"
+
+    return jsonify(ret_value)
+
+
+@company_blue.route('/GetCompanyContact', methods=['GET', ])
+def get_comp_contact():
+    """ Получает данные по компании из поля sac3.companycontact """
+
+    ret_value = {"RESULT": "ERROR", "DESC": "", "DATA": ""}
+
+    user_ip = request.remote_addr
+    logger.info(f"Запрос от ip: {user_ip}", print_it=False)
+
+    # Проверяем разрешен ли доступ для IP
+    if not ALLOW_IP.find(user_ip, logger):
+        ret_value["DESC"] = ERROR_ACCESS_IP
+    else:
+        json_request: dict = request.json
+
+        if json_request:
+            data = {'FAccountID': json_request.get('id'),
+                    'FINN': json_request.get('inn')
+                    }
+            ret_value = CompanyDB.get_contact(data)
 
         else:
            logger.error(f"Не удалось получить данные из JSON: {json_request}")
